e5f90f86d74bcbdbc32db95191b2e0315defed4b
threezinedine <threezinedine@gmail.com>
2023-04-06 04:12:32 +0700

    small update in version

--- example.cpp
+++ example.cpp
@@ -6,11 +6,32 @@
 
 
 int main(int argc, char** argv) {
-    std::cout << "CommandLine Hanlder Version: " << COMMANDLINE_VERSION_MAJOR << "." << COMMANDLINE_VERSION_MINOR << "." << COMMANDLINE_VERSION_PATCH << std::endl;
+    std::cout << "CommandLine Hanlder Version: " << VERSION << std::endl;
     TTC::CommandLineHandler handler(argc, argv);
-    handler.addProperty(TTC::PropertyFactory().create(TTC::PropertyType::INTEGER, "money", "m", "Integer property"));
-    handler.addProperty(TTC::PropertyFactory().create(TTC::PropertyType::FLOAT, "credit", "c", "Float property"));
-    handler.addProperty(TTC::PropertyFactory().create(TTC::PropertyType::STRING, "name", "n", "String property"));
+    handler.addProperty(
+        TTC::PropertyFactory().create(
+            TTC::PropertyType::INTEGER, 
+            "money", 
+            "m", 
+            "Integer property"
+        )
+    );
+    handler.addProperty(
+        TTC::PropertyFactory().create(
+            TTC::PropertyType::FLOAT, 
+            "credit", 
+            "c", 
+            "Float property"
+        )
+    );
+    handler.addProperty(
+        TTC::PropertyFactory().create(
+            TTC::PropertyType::STRING, 
+            "name", 
+            "n", 
+            "String property"
+        )
+    );
     handler.parse();
     std::cout << "Money: " << handler.getIntegerPropertyValue("money") << std::endl;
     std::cout << "Credit: " << handler.getFloatPropertyValue("credit") << std::endl;
